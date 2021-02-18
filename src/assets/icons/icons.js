78ec39005f76d26ee48cc45a8ed795c8b94d4cd1
Caio <caiookb@gmail.com>
2021-02-18 03:01:21 -0300

    feature(Icons): Created Icons assets for app

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,21 @@
+const backend = require("./backend.png").default;
+const close = require("./close.png").default;
+const edit = require("./edit.png").default;
+const email = require("./email.png").default;
+const frontend = require("./frontend.png").default;
+const mobile = require("./mobile.png").default;
+const plus = require("./plus.png").default;
+const trash = require("./trash.png").default;
+const search = require("./search.png").default;
+
+export const Icons = {
+  backend,
+  close,
+  edit,
+  email,
+  frontend,
+  mobile,
+  plus,
+  trash,
+  search,
+};
