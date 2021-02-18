a328629ed1b90e5f8921c61d35a11424a9b88b01
Caio <caiookb@gmail.com>
2021-02-18 03:03:33 -0300

    feature(Utils/Dependencies): Created Colors file and updated package.json file with new dependencies

---
+++ src/utils/Colors.js
@@ -0,0 +1,10 @@
+export const Colors = {
+  primary: "#FFFFFF",
+  red: "#E93D3D",
+  gray: "#EAEAEA",
+  green: "#4DCA61",
+  frontend: "#F7DF1E",
+  mobile: "#222222",
+  backend: "#660A0E",
+  grayText: "#6B6B6B",
+};
