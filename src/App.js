7b4ca3c60416d223ac1aab138130375d68b9af48
Caio <caiookb@gmail.com>
2021-02-19 19:25:18 -0300

    feature(App): updating dependencies, main view and components exports

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Card, TextInput, Icon, Button } from "./components";
+import { Card, TextInput, Icon, Button, Grid } from "./components";
+import Main from "./views/Main/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <Button title={"Add new employee"} color={"green"} icon={"plus"} />
-      <br></br>
+  return <Main />;
+  <div className="App">
+    <Button title={"Add new employee"} color={"green"} icon={"plus"} />
+    <br></br>
+    <Button size={"sm"} title={"Delete"} color={"red"} />
+    <br></br>
+    <Button size={"sm"} title={"Register"} color={"green"} />
+    <br></br>
+    <Button size={"sm"} title={"Cancel"} color={"green"} />
+    <br></br>
+    <Icon size={"sm"} icon={"email"} />
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <TextInput label={"Name *"} />
+    <TextInput label={"Team"} />
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
 
-      <Button size={"sm"} title={"Delete"} color={"red"} />
-      <br></br>
-
-      <Button size={"sm"} title={"Register"} color={"green"} />
-      <br></br>
-
-      <Button size={"sm"} title={"Cancel"} color={"green"} />
-      <br></br>
-
-      <Icon size={"sm"} icon={"email"} />
-      <br></br>
-
+    <Grid columns={[2]} display={[5, 1]}>
       <TextInput icon={"search"} placeholder={"Search by name or team... "} />
-      <br></br>
-      <Card />
-    </div>
-  );
+      <Button title={"Add new employee"} color={"green"} icon={"plus"} />
+    </Grid>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <Grid columns={[1, 2, 4]}>
+      <TextInput label={"Name *"} />
+      <TextInput label={"Email *"} type={"email"} />
+      <TextInput label={"CPF *"} />
+      <TextInput label={"Gender *"} type={"date"} />
+      <TextInput label={"Birthdate *"} type={"date"} />
+      <TextInput label={"Start date *"} type={"month"} />
+      <TextInput label={"Team "} />
+    </Grid>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <Card />
+  </div>;
 }
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Button from "./Buttons/Button";
 import Card from "./Card/Card";
+import Grid from "./Grid/Grid";
 import Icon from "./Icons/Icon";
 import TextInput from "./TextInput/TextInput";
 
-export { Button, Card, Icon, TextInput };
+export { Button, Card, Grid, Icon, TextInput };
